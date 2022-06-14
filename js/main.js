45ca1a8083f957ef5af3cd746fc72af0840a5ee9
TreLyn <trelyn.dev@gmail.com>
2022-06-13 22:33:17 -0500

    today's challenge

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 // Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
 
-function greet(name){
-  return `Hello, ${name} how are you doing today?`;
-}
+// function greet(name){
+//   return `Hello, ${name} how are you doing today?`;
+// }
+
+// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+// function smash (words) {
+//   let smash = ['hello', 'world', 'this', 'is', 'great']
+//
+//
+//    return "smash"
+// };
+
+// Write a function "greet" that returns "hello world!"
+  function greet(){
+    return 'hello world!'
+  }
