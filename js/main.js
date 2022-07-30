5d70845c50a226d366d480681dd686bff320ef34
TreLyn <trelyn.dev@gmail.com>
2022-07-30 11:16:03 -0500

    Unordered String

--- js/main.js
+++ js/main.js
@@ -167,18 +167,35 @@
 // }
 // You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
 
-function getMiddle(s)
-{
-  	let position;
-		let length;
+// function getMiddle(s)
+// {
+//   	let position;
+// 		let length;
+//
+// 			if(s.length % 2 == 1) {
+// 				position = s.length / 2;
+// 				length = 1;
+// 			} else {
+// 				position = s.length / 2 - 1;
+// 				length = 2;
+// 			}
+//
+// 			return s.substring(position, position + length)
+// 		}
 
-			if(s.length % 2 == 1) {
-				position = s.length / 2;
-				length = 1;
-			} else {
-				position = s.length / 2 - 1;
-				length = 2;
-			}
+//Unordered string
+//Count the number of occurrences of each character and return it as a list of tuples in order of appearance. For empty output return an empty list.
 
-			return s.substring(position, position + length)
-		}
+var orderedCount = function (text) {
+   // Implement me!
+  const splitStr = text.split('');
+  const str = [...new Set(splitStr)];
+
+  let resultArr = [];
+
+  for (let i = 0; i <= str.length-1; i += 1) {
+    resultArr.push([str[i], splitStr.filter((s) => s === str[i]).length]);
+  }
+
+  return resultArr;
+}
