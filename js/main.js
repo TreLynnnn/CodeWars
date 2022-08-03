988e42906fc6a1ab30468c854b0aaff36765d39f
TreLyn <trelyn.dev@gmail.com>
2022-08-03 07:37:50 -0500

    Keep up thee hoop

--- js/main.js
+++ js/main.js
@@ -206,10 +206,25 @@
 //
 // Random tests go up to n = 500000.
 
-let output = 0;
-    for (let i=1; i<=n; i++) {
-      if (n % i == 0) {
-        output += 1;
+// let output = 0;
+//     for (let i=1; i<=n; i++) {
+//       if (n % i == 0) {
+//         output += 1;
+// }
+// }
+//   return output;
+
+// Alex just got a new hula hoop, he loves it but feels discouraged because his little brother is better than him
+//
+// Write a program where Alex can input (n) how many times the hoop goes round and it will return him an encouraging message :)
+//
+// If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
+// If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+
+function hoopCount (n) {
+   if(n >= 10){
+     return "Great, now move on to tricks"
+   }else{
+     return "Keep at it until you get it"
+   }
 }
-}
-  return output;
