7b584124a537581f36451f6e601bcc4b110cb757
TreLyn <trelyn.dev@gmail.com>
2022-08-03 22:20:30 -0500

    Integers to Strings

--- js/main.js
+++ js/main.js
@@ -221,10 +221,15 @@
 // If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
 // If he doesn't get 10 hoops, return the string "Keep at it until you get it".
 
-function hoopCount (n) {
-   if(n >= 10){
-     return "Great, now move on to tricks"
-   }else{
-     return "Keep at it until you get it"
-   }
+// function hoopCount (n) {
+//    if(n >= 10){
+//      return "Great, now move on to tricks"
+//    }else{
+//      return "Keep at it until you get it"
+//    }
+// }
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string)
+
+function sumStr(a,b) {
+  return String(Number(a)+Number(b));
 }
