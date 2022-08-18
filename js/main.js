fcbba68c4493ab87406aa9652629282f219f8c99
TreLyn <trelyn.dev@gmail.com>
2022-08-18 09:21:13 -0500

    2022

--- js/main.js
+++ js/main.js
@@ -261,6 +261,17 @@
 
 // Create a phone number
 // Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
-function createPhoneNumber(numbers){
-  return numbers.join('').replace(/(\d{3})(\d{3})(\d{4})/,'($1) $2-$3');
-}
+// function createPhoneNumber(numbers){
+//   return numbers.join('').replace(/(\d{3})(\d{3})(\d{4})/,'($1) $2-$3');
+// }
+
+// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+function removeChar(str){
+ //You got this!
+ let splitStr = str.split('')
+ let removeFirst = splitStr.pop()
+ let removeLast = splitStr.shift()
+ const joinStr = splitStr.join('')
+ return joinStr
+};
