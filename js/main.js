03bfe1d35034ee77288b4b421d35656fc3755a0d
TreLyn <trelyn.dev@gmail.com>
2022-08-19 11:21:08 -0500

    Set an Alarm

--- js/main.js
+++ js/main.js
@@ -234,8 +234,20 @@
 //   return String(Number(a)+Number(b));
 // }
  // Given an array of integers your solution should find the smallest integer.
-class SmallestIntegerFinder {
-  findSmallestInt(args) {
-    return Math.min(...args)
-  }
+// class SmallestIntegerFinder {
+//   findSmallestInt(args) {
+//     return Math.min(...args)
+//   }
+// }
+
+// Write a function named setAlarm which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+//
+// The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise.
+
+function setAlarm(employed, vacation){
+ if(employed == true && vacation == false){
+   return true
+ }else{
+   return false
+ }
 }
