7b416abb2de7c231918ec27ec267d0ca7c75f0f4
TreLyn <trelyn.dev@gmail.com>
2022-09-15 09:34:39 -0500

    updated answer

--- js/SumOfNumbersSeries3.js
+++ js/SumOfNumbersSeries3.js
@@ -1,6 +1,12 @@
 // Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
 
 // Note: a and b are not ordered!
-function getSum( a,b ){
-    return a + b
-  }
+function getSum( a,b )
+{ if (a === b){
+return a;
+}
+let addition = a + b;
+
+return addition;
+
+}
