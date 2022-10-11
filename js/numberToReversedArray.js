fb582fea97a13d51f8edbd328e75a28c4134a04d
TreLyn <trelyn.dev@gmail.com>
2022-10-11 10:09:28 -0500

    Return the century each year is in

---
+++ js/numberToReversedArray.js
@@ -0,0 +1,19 @@
+// Convert number to reversed array of digits
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+// Example(Input => Output):
+// 35231 => [1,3,2,5,3]
+// 0 => [0]
+
+// p: function will be given a non-negative number
+// r: Should return the numbers in an array and in reversed order
+// e: 911 = [1,1,9]; 825 = [5,2,8]; 1000 = [0001];
+// p: 
+
+// create function that accepts non-negative number
+function digitize(n){
+    let numToStr = n.toString()
+    let splitStr = numToStr.split(" ")
+    console.log(splitStr)
+}
+digitize(321);
