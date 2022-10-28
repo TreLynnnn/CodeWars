606b4138d8ef4310388be6a60e7075caed38df5d
TreLyn <trelyn.dev@gmail.com>
2022-10-28 09:01:12 -0500

    Grab first letter in each name

---
+++ js/two-word-name.js
@@ -0,0 +1,10 @@
+// Abbreviate a Two Word Name
+
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+function abbrevName(name){
+    // grab the first letter in each name and return them with dot in between
+   return name.split(' ').map( name => name[0]).join(".")
+}
