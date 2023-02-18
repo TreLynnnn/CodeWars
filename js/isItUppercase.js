0c317f141bb8b5fe214e64e9acf65d179d30fea2
TreLyn <trelyn.dev@gmail.com>
2023-02-18 11:59:53 -0600

    determine is string is uppercase

---
+++ js/isItUppercase.js
@@ -0,0 +1,12 @@
+// Create a method to see whether the string is ALL CAPS.
+
+function isUpperCase(str){
+    // if (str === str.toUpperCase()) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+
+    return str === str.toUpperCase() ? true : false ;
+}
+console.log(isUpperCase('DOG'), true)
